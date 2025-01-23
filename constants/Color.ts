25dc1f74ed661b37c690fa462a2fcc1c424ca919
JaneChun <janechun22@gmail.com>
2025-01-23 19:39:13 +0900

    컬러셋 추가

---
+++ constants/Color.ts
@@ -0,0 +1,19 @@
+export const Colors = {
+	base: '#f9fafc',
+	dark_gray: '#4B5563',
+	primary: '#4BC0C0',
+
+	//border
+	border_gray: '#e8eaee',
+
+	// font
+	font_black: '#000000',
+	font_gray: '#808693',
+	font_light_gray: '#a2a9b5',
+
+	// button
+	green_background: '#d6f3db',
+	green_text: '#278734',
+	blue_background: '#d2f4ff',
+	blue_text: '#4e9eb6',
+};
