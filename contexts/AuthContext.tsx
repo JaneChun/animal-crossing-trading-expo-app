f035ab33ca05a6542f1f35e3e7424eecf3f8775b
JaneChun <janechun22@gmail.com>
2025-02-03 16:27:36 +0900

    로그인 로직 수정

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -6,7 +6,11 @@ import {
 	useState,
 } from 'react';
 import { auth } from '../fbase';
-import { OAuthProvider, signInWithCredential } from 'firebase/auth';
+import {
+	OAuthProvider,
+	onAuthStateChanged,
+	signInWithCredential,
+} from 'firebase/auth';
 import {
 	// getKeyHashAndroid,
 	initializeKakaoSDK,
@@ -18,6 +22,7 @@ import {
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type UserInfo = {
+	uid: string;
 	displayName: string;
 	photoUrl: string;
 } | null;
@@ -34,12 +39,36 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
 	const [userInfo, setUserInfo] = useState<UserInfo>(null);
 
+	// KakaoSDK 초기화
 	useEffect(() => {
 		if (process.env.EXPO_PUBLIC_KAKAO_IOS_KEY) {
 			initializeKakaoSDK(process.env.EXPO_PUBLIC_KAKAO_IOS_KEY);
 		}
 	}, []);
 
+	// Firebase Auth 상태 변화 감지
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, async (user) => {
+			if (user) {
+				const { uid, displayName, photoURL } = user;
+
+				const loginUserInfo = {
+					uid,
+					displayName: displayName ?? '',
+					photoUrl: photoURL ?? '',
+				};
+
+				setUserInfo(loginUserInfo);
+				await AsyncStorage.setItem('@user', JSON.stringify(loginUserInfo));
+			} else {
+				setUserInfo(null);
+				await AsyncStorage.removeItem('@user');
+			}
+		});
+		return () => unsubscribe(); // 언마운트 시 구독 해제
+	}, []);
+
+	// 앱 시작 시 AsyncStorage에서 유저 정보 로드
 	useEffect(() => {
 		const loadUser = async () => {
 			const storedUser = await AsyncStorage.getItem('@user');
@@ -51,7 +80,7 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
 		loadUser();
 	}, []);
 
-	const login = async () => {
+	const login = async (): Promise<boolean> => {
 		try {
 			const kakaoTokenInfo = await kakaoLogin();
 			const provider = new OAuthProvider('oidc.kakao');
@@ -63,10 +92,11 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
 			const result = await signInWithCredential(auth, credential);
 
 			const {
-				user: { displayName, photoURL },
+				user: { uid, displayName, photoURL },
 			} = result;
 
 			const loginUserInfo = {
+				uid,
 				displayName: displayName ?? '',
 				photoUrl: photoURL ?? '',
 			};
@@ -74,21 +104,26 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
 			// 상태 업데이트 & AsyncStorage에서 삭제
 			setUserInfo(loginUserInfo);
 			await AsyncStorage.setItem('@user', JSON.stringify(loginUserInfo));
+
+			return true;
 		} catch (error) {
-			console.log('Login failed:', error);
+			console.log('로그인 실패:', error);
 			setUserInfo(null);
+
+			return false;
 		}
 	};
 
 	const logout = async () => {
 		try {
-			await kakaoLogout();
+			await kakaoLogout(); // 카카오 로그아웃
+			await auth.signOut(); // Firebase auth 로그아웃
 
 			// 상태 업데이트 & AsyncStorage에서 삭제
 			setUserInfo(null);
 			await AsyncStorage.removeItem('@user');
 		} catch (error) {
-			console.log('Logout failed:', error);
+			console.log('로그아웃 실패:', error);
 		}
 	};
 
@@ -102,7 +137,7 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
 export const useAuthContext = () => {
 	const context = useContext(AuthContext);
 	if (!context) {
-		throw new Error('useAuth must be used within an AuthProvider');
+		throw new Error('useAuth는 AuthProvider 안에서 사용되어야 합니다.');
 	}
 	return context;
 };
--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,4 +1,11 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import {
+	View,
+	Text,
+	StyleSheet,
+	Image,
+	TouchableOpacity,
+	Alert,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import type { TabNavigation } from '@/types/navigation';
 
@@ -8,9 +15,14 @@ const Login = () => {
 	const { userInfo, login } = useAuthContext();
 	const navigation = useNavigation<TabNavigation>();
 
-	const handleLogin = () => {
-		login();
-		navigation.navigate('Home');
+	const handleLogin = async () => {
+		const isSuccess: boolean | void = await login();
+
+		if (Boolean(isSuccess)) {
+			navigation.navigate('Home');
+		} else {
+			Alert.alert('로그인 실패', '다시 시도해주세요.');
+		}
 	};
 
 	return (
