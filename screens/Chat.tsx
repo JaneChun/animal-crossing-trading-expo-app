811a77a8f41559307927a4c6ceca8656293b17d3
JaneChun <janechun22@gmail.com>
2025-04-02 17:14:37 +0900

    style: 채팅 없음 메시지 상단에 아이콘 추가

--- screens/Chat.tsx
+++ screens/Chat.tsx
@@ -5,6 +5,7 @@ import useGetChats from '@/hooks/useGetChats';
 import useLoading from '@/hooks/useLoading';
 import { useAuthStore } from '@/stores/AuthStore';
 import { ChatWithReceiverInfo } from '@/types/chat';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import React from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 
@@ -25,6 +26,12 @@ const Chat = () => {
 		<Layout title='채팅'>
 			{!userInfo || chats.length === 0 ? (
 				<View style={styles.emptyContainer}>
+					<MaterialCommunityIcons
+						name='message-processing-outline'
+						color={Colors.font_light_gray}
+						size={72}
+						style={styles.emptyIcon}
+					/>
 					<Text style={styles.emptyText}>참여 중인 채팅방이 없습니다.</Text>
 				</View>
 			) : (
@@ -41,8 +48,12 @@ const Chat = () => {
 const styles = StyleSheet.create({
 	emptyContainer: {
 		flex: 1,
+		flexDirection: 'column',
 		justifyContent: 'center',
 		alignItems: 'center',
+	},
+	emptyIcon: {
+		marginBottom: 16,
 	},
 	emptyText: {
 		fontSize: 14,
