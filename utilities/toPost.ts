4deece922219e80fd211ddcebd8cf75546b29f79
JaneChun <janechun22@gmail.com>
2025-04-20 18:02:10 +0900

    fix: toPost 타입 오류 수정

--- utilities/toPost.ts
+++ utilities/toPost.ts
@@ -1,13 +1,39 @@
-import { Post, PostDoc } from '@/types/post';
+import {
+	CartItem,
+	Collection,
+	CommonPostFields,
+	CommunityType,
+	MarketType,
+	Post,
+	PostDoc,
+} from '@/types/post';
 
-export const toPost = (doc: PostDoc): Post => ({
-	id: doc.id,
-	type: doc.type,
-	title: doc.title,
-	body: doc.body,
-	cart: doc.cart,
-	images: doc.images,
-	creatorId: doc.creatorId,
-	createdAt: doc.createdAt,
-	commentCount: doc.commentCount,
-});
+export const toPost = <C extends Collection>(
+	collectionName: C,
+	doc: PostDoc<C>,
+): Post<C> => {
+	const commonFields: CommonPostFields = {
+		id: doc.id,
+		title: doc.title,
+		body: doc.body,
+		creatorId: doc.creatorId,
+		createdAt: doc.createdAt,
+		commentCount: doc.commentCount,
+	};
+
+	if (collectionName === 'Boards') {
+		const boardDoc = doc as PostDoc<'Boards'>;
+		return {
+			...commonFields,
+			type: boardDoc.type as MarketType,
+			cart: boardDoc.cart as CartItem[],
+		} as Post<C>;
+	}
+
+	const communityDoc = doc as PostDoc<'Communities'>;
+	return {
+		...commonFields,
+		type: communityDoc.type as CommunityType,
+		images: communityDoc.images,
+	} as Post<C>;
+};
