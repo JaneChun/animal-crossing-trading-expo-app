d2b878b725a1840341ada06c5c2447685f0a7a62
JaneChun <janechun22@gmail.com>
2025-06-21 00:12:58 +0900

    feat: 동일 유저에 대한 중복 신고 제한 로직 추가

--- firebase/services/reportService.ts
+++ firebase/services/reportService.ts
@@ -1,19 +1,50 @@
+import { db } from '@/fbase';
 import { CreateReportRequest } from '@/types/report';
-import { Timestamp } from 'firebase/firestore';
+import { collection, query, Timestamp, where } from 'firebase/firestore';
 import firestoreRequest from '../core/firebaseInterceptor';
-import { addDocToFirestore } from '../core/firestoreService';
+import {
+	addDocToFirestore,
+	queryDocs,
+	updateDocToFirestore,
+} from '../core/firestoreService';
 
 export const createReport = async (requestData: CreateReportRequest) => {
 	return firestoreRequest('유저 신고', async () => {
-		const createdId = await addDocToFirestore({
-			directory: 'Reports',
-			requestData: {
+		const { reporterId, reporteeId } = requestData;
+
+		const q = query(
+			collection(db, 'Reports'),
+			where('reporterId', '==', reporterId),
+			where('reporteeId', '==', reporteeId),
+		);
+
+		const existingReports = await queryDocs(q);
+
+		if (existingReports.length === 0) {
+			// 새 신고 생성
+			const createdId = await addDocToFirestore({
+				directory: 'Reports',
+				requestData: {
+					...requestData,
+					createdAt: Timestamp.now(),
+					resolved: false,
+				},
+			});
+
+			return createdId;
+		} else {
+			// 기존 신고 문서 갱신
+			const updatedReportData = {
 				...requestData,
 				createdAt: Timestamp.now(),
 				resolved: false,
-			},
-		});
+			};
 
-		return createdId;
+			updateDocToFirestore({
+				collection: 'Reports',
+				id: existingReports[0].id,
+				requestData: updatedReportData,
+			});
+		}
 	});
 };
