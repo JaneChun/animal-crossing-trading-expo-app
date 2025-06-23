afb9ec8596f26c19dcf5f0a948aaa58395b69f3b
JaneChun <janechun22@gmail.com>
2025-06-23 21:51:59 +0900

    feat: profileFormSchema에 비속어 필터링 추가 및 유효성 검사 에러 메시지 표시

--- hooks/form/Profile/profileFormSchema.ts
+++ hooks/form/Profile/profileFormSchema.ts
@@ -1,3 +1,4 @@
+import cenkor from 'cenkor';
 import { ImagePickerAsset } from 'expo-image-picker';
 import * as z from 'zod';
 
@@ -9,7 +10,10 @@ export const ProfileFormSchema = z.object({
 		.regex(
 			/^[a-zA-Z0-9가-힣]+$/,
 			'닉네임은 띄어쓰기 없이 한글, 영문, 숫자만 가능합니다.',
-		),
+		)
+		.refine((name) => !cenkor(name).filtered, {
+			message: '닉네임에 부적절한 단어가 포함되어 있습니다.',
+		}),
 	islandName: z
 		.string()
 		.min(2, '섬 이름을 입력해주세요.')
@@ -18,6 +22,9 @@ export const ProfileFormSchema = z.object({
 			/^[a-zA-Z0-9가-힣]+$/,
 			'섬 이름은 띄어쓰기 없이 한글, 영문, 숫자만 가능합니다.',
 		)
+		.refine((name) => !cenkor(name).filtered, {
+			message: '섬 이름에 부적절한 단어가 포함되어 있습니다.',
+		})
 		.refine((value) => value.endsWith('섬') || value.endsWith('도'), {
 			message: '섬 이름은 "섬" 또는 "도"로 끝나야 합니다.',
 		}),
