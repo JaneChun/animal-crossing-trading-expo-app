ec378c554779b6331bb84fe96bdd1ab805bfe122
JaneChun <janechun22@gmail.com>
2025-08-29 20:52:19 +0900

    feat: 프로필 작성한 글 타입 필터 기능 추가

--- components/Profile/MyPosts.tsx
+++ components/Profile/MyPosts.tsx
@@ -1,24 +1,79 @@
+import { Colors } from '@/constants/Color';
+import { FontSizes, FontWeights } from '@/constants/Typography';
 import { MyPostsProps } from '@/types/components';
-import React from 'react';
+import { Collection } from '@/types/post';
+import { useActionSheet } from '@expo/react-native-action-sheet';
+import { Entypo } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text } from 'react-native';
 import PostList from '../Home/PostList';
 import Layout, { PADDING } from '../ui/layout/Layout';
 
 const MyPosts = ({ isMyProfile, profileInfo }: MyPostsProps) => {
+	const { showActionSheetWithOptions } = useActionSheet();
+	const [selectedCollection, setSelectedCollection] = useState<Collection>('Boards');
+
+	const options = [
+		{ value: 'Boards' as Collection, label: '마켓' },
+		{ value: 'Communities' as Collection, label: '커뮤니티' },
+	];
+
+	const selectedOption = options.find((option) => option.value === selectedCollection);
+
+	const showOptionSelect = () => {
+		const actionOptions = [...options.map(({ label }) => label), '닫기'];
+
+		showActionSheetWithOptions(
+			{
+				options: actionOptions,
+				cancelButtonIndex: actionOptions.length - 1,
+			},
+			(buttonIndex) => {
+				if (
+					buttonIndex === undefined ||
+					!options[buttonIndex] ||
+					buttonIndex === actionOptions.length - 1
+				)
+					return;
+
+				setSelectedCollection(options[buttonIndex].value);
+			},
+		);
+	};
+
 	return (
 		<Layout
-			title={
-				isMyProfile ? '나의 거래글' : `${profileInfo.displayName}님의 거래글`
-			}
+			title={isMyProfile ? '작성한 글' : `${profileInfo.displayName}님의 글`}
 			containerStyle={{ paddingTop: PADDING }}
 			headerStyle={{ paddingBottom: 0 }}
+			headerRightComponent={
+				<Pressable style={styles.filterButtonContainer} onPress={showOptionSelect}>
+					<Text style={styles.filterButtonText}>{selectedOption?.label}</Text>
+					<Entypo name='chevron-down' size={16} color={Colors.font_gray} />
+				</Pressable>
+			}
 		>
 			<PostList
-				collectionName='Boards'
+				collectionName={selectedCollection}
 				filter={{ creatorId: profileInfo?.uid }}
 				containerStyle={{ paddingHorizontal: PADDING }}
 			/>
 		</Layout>
 	);
 };
+
+const styles = StyleSheet.create({
+	filterButtonContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		padding: 6,
+	},
+	filterButtonText: {
+		fontSize: FontSizes.sm,
+		fontWeight: FontWeights.semibold,
+		color: Colors.font_gray,
+		marginRight: 2,
+	},
+});
 
 export default MyPosts;
