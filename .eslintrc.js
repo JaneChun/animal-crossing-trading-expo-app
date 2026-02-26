7c2a636efd1c1155c3efb7c9ddfbf1afe0b90cb8
JaneChun <janechun22@gmail.com>
2026-02-26 15:00:29 +0900

    chore: ESLint에 react/jsx-runtime 플러그인 추가 및 lint:fix 스크립트 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
 		'expo',
 		'eslint:recommended',
 		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
 		'plugin:react-hooks/recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:import/errors',
