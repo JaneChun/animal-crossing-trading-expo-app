209471d81195fd1f1890628a01be56773754f5ea
JaneChun <janechun22@gmail.com>
2026-03-03 19:27:31 +0900

    chore: functions ESLint 설정 수정 및 테스트 파일 미사용 코드 정리

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,28 +1,41 @@
 module.exports = {
 	root: true,
 	env: {
-		es6: true,
+		es2021: true,
 		node: true,
 	},
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
 	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		sourceType: 'module',
-	},
-	plugins: ['@typescript-eslint'],
-	ignorePatterns: ['/lib/**/*', '/node_modules/**/*'],
-	rules: {
-		'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-		'@typescript-eslint/no-explicit-any': 'warn',
-		'@typescript-eslint/no-var-requires': 'off',
-	},
-	overrides: [
-		{
-			files: ['**/__tests__/**/*.ts', '**/*.test.ts'],
-			env: { jest: true },
-			rules: {
-				'@typescript-eslint/no-empty-function': 'off',
-			},
-		},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier', // 포맷팅 충돌 방지
 	],
+	plugins: ['@typescript-eslint', 'unused-imports'],
+	ignorePatterns: [
+		'/lib/**/*', // 빌드 결과물 제외
+		'/node_modules/**/*',
+		'/__tests__/**/*',
+		'*.test.ts',
+		'*.spec.ts',
+	],
+	rules: {
+		// 1. 타입 관련 룰 완화
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off', // 함수 리턴 타입 강제 안 함
+		'@typescript-eslint/no-inferrable-types': 'off',
+		'@typescript-eslint/ban-ts-comment': 'off', // @ts-ignore 허용
+
+		// 2. 비동기 작업 안정성
+		'no-async-promise-executor': 'warn',
+		'no-await-in-loop': 'warn',
+
+		// 3. 미사용 변수 및 정리
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'unused-imports/no-unused-imports': 'error',
+		'unused-imports/no-unused-vars': [
+			'warn',
+			{ vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+		],
+	},
 };
